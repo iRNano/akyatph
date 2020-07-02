097066bffe50dc4caf637561e31bbfc47d89e632
Adrian Valdepenas <valdepenas.adrian@gmail.com>
2020-07-02 17:48:45 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import L from 'leaflet';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import greenIcon from './assets/icons/leaf-green.png'
+import redIcon from './assets/icons/leaf-red.png'
+import orangeIcon from './assets/icons/leaf-orange.png'
+import shadowIcon from './assets/icons/leaf-shadow.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+var greenMarker = L.icon({
+  iconUrl: greenIcon,
+  shadowUrl: shadowIcon,
+
+  iconSize:     [38, 95], // size of the icon
+  shadowSize:   [50, 64], // size of the shadow
+  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
+class App extends Component {
+
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
+
+  render(){
+    const position= [this.state.lat, this.state.lng];
+    return (
+      <Map id="map" center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} icon={greenMarker}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+  
 }
 
 export default App;
